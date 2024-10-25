3cb3443c2b4467e13eaa65fdcb39215d5052806b
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-10-25 18:14:18 +0545

    Refactor supabaseClient to use environment variables for SUPABASE_URL and SUPABASE_KEY

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabseURL = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabseURL = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabseURL || !supabaseKey) {
     throw new Error("Please provide SUPABASE_URL and SUPABASE_KEY in the environment variables");
 }
 
-export const supabase = createClient("https://cpcqjoamwpspnryfgpzz.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwY3Fqb2Ftd3BzcG5yeWZncHp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyMjgxNzksImV4cCI6MjA0NDgwNDE3OX0.DijT8bIL_77ATerj9ciguWxkL5PpIq2F40o6lxVUe9c");
+export const supabase = createClient(supabseURL, supabaseKey);
 
 export const signOut = async () => {
     const { error } = await supabase.auth.signOut()

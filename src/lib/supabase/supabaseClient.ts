1fb82a0058e1554380462dc544ae707a46990565
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-10-25 18:18:54 +0545

    Refactor supabaseClient to use Vite environment variables

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -1,11 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabseURL = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-if (!supabseURL || !supabaseKey) {
-    throw new Error("Please provide SUPABASE_URL and SUPABASE_KEY in the environment variables");
-}
+const supabseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabseURL, supabaseKey);
 
